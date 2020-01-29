d428e35f43233a071221df44967729be0d24ebcc
maxkorzan <maxkorzan@gmail.com>
2020-01-29 14:43:57 -0500

    updated Max class

--- src/Max.java
+++ src/Max.java
@@ -1,25 +1,13 @@
 import java.util.*;
 
 public class Max {
+
+    static HashMap<Integer, String> roomNames = new HashMap<>();
+    static HashMap<Integer, String> roomContains = new HashMap<>();
+    static HashMap<Integer, String> roomDirections = new HashMap<>();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-//////////////////////////////////////////////////////////////////////////
-        welcome();
-        room(1);
-        String direction = getDirection();
-        switch (direction){
-            case "N": room(1);
-            case "S": room(2);
-            case "E": room(6);
-            case "W": room(4);
-
-            default:
-                System.out.println("Invalid input");
-                getDirection();
-        }
-
-//////////////////////////////////////////////////////////////////////////
-        HashMap<Integer, String> roomNames = new HashMap<>();
         roomNames.put(1, "foyer");
         roomNames.put(2, "front room");
         roomNames.put(3, "library");
@@ -29,7 +17,6 @@ public class Max {
         roomNames.put(7, "parlor");
         roomNames.put(8, "secret room");
 
-        HashMap<Integer, String> roomContains = new HashMap<>();
         roomContains.put(1, "dead scorpion");
         roomContains.put(2, "piano");
         roomContains.put(3, "spider");
@@ -39,17 +26,36 @@ public class Max {
         roomContains.put(7, "treasure chest");
         roomContains.put(8, "piles of gold");
 
-        HashMap<Integer, String> roomDirections = new HashMap<>();
-        roomDirections.put(1, "N =  Room2");
-        roomDirections.put(2, "S = roomName.get(1), W = Room 3, E = Room 4");
-        roomDirections.put(3, "");
-        roomDirections.put(4, "");
-        roomDirections.put(5, "");
-        roomDirections.put(6, "");
-        roomDirections.put(7, "");
-        roomDirections.put(8, "");
+        roomDirections.put(1, "North");
+        roomDirections.put(2, "South, West, or East");
+        roomDirections.put(3, "East or North");
+        roomDirections.put(4, "West or North");
+        roomDirections.put(5, "South");
+        roomDirections.put(6, "East");
+        roomDirections.put(7, "West or South");
+        roomDirections.put(8, "West");
+
+        Scanner sc = new Scanner(System.in);
+//////////////////////////////////////////////////////////////////////////
+
+        welcome();
+        foyer();
+//        room(1);
+//        String direction = getDirection();
+//        switch (direction){
+//            case "N": room(1);
+//            case "S": room(2);
+//            case "E": room(6);
+//            case "W": room(4);
+//
+//            default:
+//                System.out.println("Invalid input");
+//                getDirection();
+//        }
+
 
     }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public static void welcome(){
         System.out.println("Welcome to CASTLE ZORK");
@@ -60,7 +66,6 @@ public class Max {
 
     public static String getDirection(){
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Which direction will you take? (N,S,E,W, or Q to quit) ");
         return sc.next();
     }
@@ -73,37 +78,74 @@ public class Max {
         System.out.println();
     }
 
-//    public static void foyer(){
-//        System.out.println();
-//        System.out.println("You have entered the foyer.");
-//        System.out.println("This room contains: ");
-//        System.out.println("Door directions: ");
-//        System.out.println();
-//    }
-//
-//    public static void frontRoom(){
-//        System.out.println();
-//        System.out.println("You have entered the front room.");
-//        System.out.println("This room contains: ");
-//        System.out.println("Door directions: ");
-//        System.out.println();
-//    }
-//
-//    public static void library(){
-//        System.out.println();
-//        System.out.println("You have entered the library.");
-//        System.out.println("This room contains: ");
-//        System.out.println("Door directions: ");
-//        System.out.println();
-//    }
-//
-//    public static void kitchen(){
-//        System.out.println();
-//        System.out.println("You have entered the kitchen.");
-//        System.out.println("This room contains: ");
-//        System.out.println("Door directions: ");
-//        System.out.println();
-//    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void foyer(){
+        System.out.println();
+        System.out.println("You have entered the foyer.");
+        System.out.println("This room contains: "+roomContains.get(1));
+        System.out.println("Door directions: "+roomDirections.get(1));
+        System.out.println();
+
+        String direction = getDirection();
+        switch (direction){
+            case "N": frontRoom();
+            default:
+                System.out.println("Invalid input");
+                getDirection();
+        }
+    }
+
+    public static void frontRoom(){
+        System.out.println();
+        System.out.println("You have entered the front room.");
+        System.out.println("This room contains: "+roomContains.get(2));
+        System.out.println("Door directions: "+roomDirections.get(2));
+        System.out.println();
+
+        String direction = getDirection();
+        switch (direction) {
+            case "S": foyer();
+            case "W": library();
+            case "E": kitchen();
+            default:
+                System.out.println("Invalid input");
+                getDirection();
+        }
+    }
+
+    public static void library(){
+        System.out.println();
+        System.out.println("You have entered the library.");
+        System.out.println("This room contains: "+roomContains.get(3));
+        System.out.println("Door directions: "+roomDirections.get(3));
+        System.out.println();
+
+        String direction = getDirection();
+        switch (direction) {
+            case "E": frontRoom();
+            case "N": diningRoom();
+            default:
+                System.out.println("Invalid input");
+                getDirection();
+        }
+    }
+
+    public static void kitchen(){
+        System.out.println();
+        System.out.println("You have entered the kitchen.");
+        System.out.println("This room contains: "+roomContains.get(4));
+        System.out.println("Door directions: "+roomDirections.get(4));
+        System.out.println();
+    }
+
+    public static void diningRoom(){
+        System.out.println();
+        System.out.println("You have entered the dining room.");
+        System.out.println("This room contains: "+roomContains.get(5));
+        System.out.println("Door directions: "+roomDirections.get(5));
+        System.out.println();
+    }
 
 
 
