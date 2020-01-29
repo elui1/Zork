84c43b75c785b91e7f9be275d58e59560ae7255b
elui1 <eric.lui2002@gmail.com>
2020-01-29 11:33:04 -0500

    first commit

---
+++ src/Eric.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class Eric {
+    public static HashMap
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> roomMap = new HashMap<>();
+        System.out.println(roomMap);
+    }
+
+    public static void foyer() {
+
+    }
+
+    public static void frontRoom() {
+
+    }
+
+    public static void library() {
+
+    }
+}
